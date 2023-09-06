d84d7027a622d544535cd753ba4b909c23999375
Fiona Walter <73806661+fwal923@users.noreply.github.com>
2023-09-06 12:10:55 +0200

    Create occupations.sql

---
+++ occupations.sql
@@ -0,0 +1,31 @@
+/*
+Task:
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation.
+The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+Input Format
+The OCCUPATIONS table is described as follows: Columns: Name, Occupation. Both of String type.
+Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+Explanation for result:
+The first column is an alphabetically ordered list of Doctor names.
+The second column is an alphabetically ordered list of Professor names.
+The third column is an alphabetically ordered list of Singer names.
+The fourth column is an alphabetically ordered list of Actor names.
+The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.
+*/
+
+select
+Doctor, Professor, Singer, Actor
+from
+(
+    select
+    row_number() over (partition by Occupation order by Name) as rn,
+    Name,
+    Occupation
+    from occupations
+) as p
+
+pivot (max(Name) for Occupation in ([Doctor], [Professor], [Singer], [Actor])) as pvt
+order by rn
