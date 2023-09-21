f3cc29e48a039a328214b5586277902373853a64
Fiona Walter <73806661+fwal923@users.noreply.github.com>
2023-09-21 21:04:30 +0200

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -13,6 +13,44 @@ where district = 'California'
 ;
 
 /*
+Revising Aggregations - Averages
+TASK:
+Query the average population of all cities in CITY where District is California.
+Input: CITY Table (can be found on Hackerrank)
+*/
+
+select
+avg(population)
+from city
+where district = 'California'
+;
+
+/*
+Aggregation - Average Population
+TASK:
+Query the average population of all cities in CITY rounded down to the nearest integer.
+Input: CITY Table (can be found on Hackerrank)
+*/
+
+select
+floor(avg(population))
+from city
+;
+
+/*
+Aggregation - Japan Population
+TASK:
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+Input: CITY Table (can be found on Hackerrank)
+*/
+
+select
+sum(population)
+from city
+where countrycode = 'JPN'
+;
+
+/*
 African City
 TASK
 Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
