0c2243dbb0af04084bde74bae345404f7709f646
Fiona Walter <73806661+fwal923@users.noreply.github.com>
2023-09-21 20:56:25 +0200

    Update and rename african_cities.sql to basics.sql

--- basics.sql
+++ basics.sql
@@ -1,4 +1,19 @@
+-- all completed in MS SQL server
 /*
+Revising Aggregations - The Sum Function
+TASK:
+Query the total population of all cities in CITY where District is California.
+Input: CITY Table (can be found on Hackerrank)
+*/
+
+select
+sum(population)
+from city
+where district = 'California'
+;
+
+/*
+African City
 TASK
 Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
@@ -13,3 +28,4 @@ city
 JOIN country on city.countrycode = country.code
 WHERE
 lower(country.continent) LIKE '%africa%' -- this accounts for data entry errors
+;
